426de9ca2a9206159f4e52557cdf6daf82d78695
Andrew <azhang1818@gmail.com>
2020-10-20 15:41:40 -0700

    Basic Template Load in

--- venv/app.py
+++ venv/app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+import projects
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return "Hello, World!"
+def home_route():
+    return render_template("home.html", projects=projects.setup())
+
+@app.route('/hello/')
+def hello_rooute():
+    return render_template("hello.html", projects=projects.setup())
 
 if __name__ == "__main__":
     app.run(debug = True, port=8080)
