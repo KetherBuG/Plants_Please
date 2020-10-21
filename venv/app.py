fb9b2968d11d589e542782305f12fc3c1e05e40d
Andrew <azhang1818@gmail.com>
2020-10-21 10:00:32 -0700

    Connect

--- venv/app.py
+++ venv/app.py
@@ -8,8 +8,12 @@ def home_route():
     return render_template("home.html", projects=projects.setup())
 
 @app.route('/hello/')
-def hello_rooute():
+def hello_route():
     return render_template("hello.html", projects=projects.setup())
+
+@app.route('/fruit&veggie')
+def fruit_route():
+    return render_template("fruit_veggie.html", projects=projects.setup())
 
 if __name__ == "__main__":
     app.run(debug = True, port=8080)
