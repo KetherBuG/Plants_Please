0ade29da0f801e94fd86eda553b324ef543848a8
Andrew <azhang1818@gmail.com>
2020-10-24 18:57:52 -0700

    Login UI Complete

--- venv/app.py
+++ venv/app.py
@@ -1,8 +1,24 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import Form, BooleanField, StringField, PasswordField
+from wtforms.validators import InputRequired,Email,Length
 import projects
 
-app = Flask(__name__)
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'AnimeisGOOD'
+Bootstrap(app)
+
+class LoginForm(FlaskForm):
+    username = StringField('username',validators=[InputRequired(), Length(min=4,max=15)])
+    password = PasswordField('password',validators=[InputRequired(), Length(min=8,max=80)])
+    remember = BooleanField('remember me')
+
+class RegisterForm(FlaskForm):
+    email = StringField('email', validators=[InputRequired(),Email(message='Invalid Email'),Length(max=50)])
+    username = StringField('username',validators=[InputRequired(), Length(min=4,max=15)])
+    password = PasswordField('password',validators=[InputRequired(), Length(min=8,max=80)])
 @app.route('/')
 def home_route():
     return render_template("home.html", projects=projects.setup())
@@ -14,9 +30,18 @@ def hello_route():
 @app.route('/fruitveggie/')
 def fruit_route():
     return render_template("fruit_veggie.html", projects=projects.setup())
-@app.route('/login/')
+
+@app.route('/login/',methods = ['GET','POST'])
 def login():
-    return render_template("index.html", projects=projects.setup())
+    form = LoginForm()
+
+    return render_template("login.html", form = form, projects=projects.setup())
+
+@app.route('/signup/',methods = ['GET','POST'])
+def signup():
+    form = RegisterForm()
+    return render_template("signup.html", form = form, projects=projects.setup())
+
 
 if __name__ == "__main__":
     app.run(debug = True, port=8080)
